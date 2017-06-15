bf2390fefe38f85cf19f1aa8ef6c4da13fcf481c
chaoos <p1020389@yahoo.com>
2017-06-15 18:41:25 +0200

    exp_euler added

--- stiff_ode_solvers.py
+++ stiff_ode_solvers.py
@@ -6,6 +6,32 @@ import scipy.optimize
 import matplotlib.pyplot as plt
 from ode_solvers import *
 
+def exp_euler(f, Df, y0, t0, T, N):
+    """
+    Exponentielles Euler Verfahren
+
+    @param {callable} f         - function
+    @param {callable} Df        - Jacobimatrix of f
+    @param {float} t0           - Anfangszeit
+    @param {float} T            - Endzeit
+    @param {ndarray|float} y0   - Anfangswert
+    @param {int} N              - Anzahl Iterationen
+
+    @return {array} t     - Zeiten
+    @return {ndarray} y   - Orte
+    """
+
+    t, h = np.linspace(t0, T, N, retstep=True)
+    y0 = np.atleast_1d(y0)
+    y = np.zeros((N, y0.shape[0]))
+    y[0,:] = y0
+
+    for k in range(N-1):
+        J = Df(y[k,:])
+        x = np.linalg.solve(J, f(y[k,:]))
+        y[k+1,:] = y[k,:] + np.dot(np.expm(h*J) - np.eye(np.size(y0)), x)
+    return t, y
+
 if __name__ == '__main__':
 
     rhs = lambda t, y: -4*y*(y - 2)
